932ac63eb6fefc7eb1fe238165349a73d26463ce
bydeuko23 <n1089932@gmail.com>
2026-05-02 14:34:43 +0300

    Update ping pong.py
    создание класса родителей

--- ping pong.py
+++ ping pong.py
@@ -1,2 +1,20 @@
 from pygame import *
 '''Необходимые классы'''
+#класс-родитель для спрайтов 
+class GameSprite(sprite.Sprite):
+    #конструктор класса
+       #конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height): # добавить еще два параметра при создании и задавать размер прямоугольгника для картинки самим
+        super().__init__()
+ 
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (wight, height)) # вместе 55,55 - параметры
+        self.speed = player_speed
+ 
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
